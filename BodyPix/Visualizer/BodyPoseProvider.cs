99740deac7f9ddf4a94b54ca9e6c22e72de43632
hungmingjyun <hungmingjyun@team-lab.com>
2022-05-18 11:55:52 +0800

    update

--- BodyPix/Visualizer/BodyPoseProvider.cs
+++ BodyPix/Visualizer/BodyPoseProvider.cs
@@ -1,12 +1,14 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace mj.gist.tracking.bodyPix {
-    public class BodyPoseProvider : MonoBehaviour {
+    public class BodyPoseProvider : SingletonMonoBehaviour<BodyPoseProvider> {
         [SerializeField] private ResourceSet resources = null;
         [SerializeField] private Vector2Int resolution = new Vector2Int(512, 384);
 
         public GraphicsBuffer KeypointBuffer => detector.KeypointBuffer;
         public RenderTexture MaskTexture => detector.MaskTexture;
+        public IEnumerable<Keypoint> Keypoints => detector.Keypoints;
 
         private ImageSource source = null;
         private BodyDetector detector;
