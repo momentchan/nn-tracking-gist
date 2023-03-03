ebee6456b48aa2c2d6c180d11d4adab1bbdd1a86
momentchan <mingjyunhung@gmail.com>
2023-03-03 12:51:35 +0800

    update

--- Common/Utility.cs
+++ Common/Utility.cs
@@ -13,21 +13,6 @@ namespace mj.gist.tracking {
         }
     }
 
-
-    static class ComputeShaderExtensions {
-        public static void DispatchThreads
-          (this ComputeShader compute, int kernel, int x, int y, int z) {
-            uint xc, yc, zc;
-            compute.GetKernelThreadGroupSizes(kernel, out xc, out yc, out zc);
-
-            x = (x + (int)xc - 1) / (int)xc;
-            y = (y + (int)yc - 1) / (int)yc;
-            z = (z + (int)zc - 1) / (int)zc;
-
-            compute.Dispatch(kernel, x, y, z);
-        }
-    }
-
     static class ColorUtil {
         public static bool IsLinear
           => QualitySettings.activeColorSpace == ColorSpace.Linear;
